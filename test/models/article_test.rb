18faa14c044a1ebe79155d340f66c132c75977a0
Charlotte Bory <cmmbory@gmail.com>
2021-03-10 18:34:04 +0100

    add second test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "article creation requires url" do
+    assert Article.create(url: nil).valid? == false
+  end
+
+  test "should not save article without url" do
+    article = Article.new
+    assert_not article.save
+  end
 end

e50ed69e5bb27611ef2fe5633d69693f5d347a33
Charlotte Bory <cmmbory@gmail.com>
2021-03-10 20:15:02 +0100

    end of day 10/03 -- parsing methode not tested

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,15 @@
+require 'uri'
+
 class ArticlesController < ApplicationController
   def create
+  end
+
+  private
+
+  def build_url(source, query)
+    parsed_query = query.split(' ').map { |word| URI.escape(word) }
+    if source == "encyclopedia.com"
+      url = "https://www.encyclopedia.com/gsearch?q=#{parsed_query}"
+    end
   end
 end

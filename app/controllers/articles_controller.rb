0d20ec1638486731b5ba692c1adf1511867ce260
Charlotte Bory <cmmbory@gmail.com>
2021-03-11 15:09:32 +0100

    list sources on submit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,26 @@
 class ArticlesController < ApplicationController
 
-    skip_before_action :authenticate_user!, only: [ :create ]
+    skip_before_action :authenticate_user!, only: [ :create, :find_sources ]
 
-  def create
-    args = get_article_info("wikipedia.com", params[:article][:query])
-    article = Article.new(args)
-    article.save
+  # def create
+  #   args = get_article_info("wikipedia.com", params[:article][:query])
+  #   article = Article.new(args)
+  #   article.save
+  # end
+
+  def find_sources
+    @articles_found = {}
+    # Check wikipedia
+    @articles_found[:wikipedia] = top_article_wikipedia(params[:article][:q])
+    render 'pages/home'
   end
 
   private
 
-  # Build article url based on selected source and input query
-  def get_article_info(source, query)
-    parsed_query = query.split(' ').map { |word| URI.escape(word) }.join('+')
-    if source == "wikipedia.com"
-      search_url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=#{parsed_query}"
-      article_info = get_top_article_wikipedia(search_url)
-    end
-    return article_info
-  end
-
   # On wikipedia.com, retrieve top search result url
-  def get_top_article_wikipedia(url)
+  def top_article_wikipedia(query)
+    parsed_query = query.split(' ').map { |word| URI.escape(word) }.join('+')
+    url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=#{parsed_query}"
     response = JSON.parse(RestClient.get(url))
     articles = response[1]
     urls = response[3]

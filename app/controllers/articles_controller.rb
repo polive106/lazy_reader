14ae237f39e95789444b5d9ee0c75fdcaff76d3b
Charlotte Bory <cmmbory@gmail.com>
2021-03-11 12:12:24 +0100

    add rest client

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 
   def create
     article = Article.new()
-    article.url = article_url("encyclopedia.com", params[:article][:query])
+    article.url = article_url("wikipedia.com", params[:article][:query])
     article.save
   end
 
@@ -16,18 +16,16 @@ class ArticlesController < ApplicationController
   # Build article url based on selected source and input query
   def article_url(source, query)
     parsed_query = query.split(' ').map { |word| URI.escape(word) }.join('+')
-    if source == "encyclopedia.com"
-      search_url = "https://www.encyclopedia.com/gsearch?q=#{parsed_query}"
-      article_url = get_top_article_encyclopedia(search_url)
+    if source == "wikipedia.com"
+      "https://en.wikipedia.org/w/index.php?search=#{parsed_query}&title=Special:Search&fulltext=1&ns0=1"
+      search_url = "https://en.wikipedia.org/w/index.php?search=#{parsed_query}&title=Special:Search&fulltext=1&ns0=1"
+      article_url = get_top_article_wikipedia(search_url)
     end
     return article_url
   end
 
-  # On encyclopedia.com, retrieve top search result url
-  def get_top_article_encyclopedia(url)
-    # TO DO - load page through headless chrome
-    page = Nokogiri::HTML(open(url))
-    top_link = page.css('a.gs-title').first
-    return top_link['href']
+  # On wikipedia.com, retrieve top search result url
+  def get_top_article_wikipedia(url)
+
   end
 end

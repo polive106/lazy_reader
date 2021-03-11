cf842556093418c71a8e7a999e9a38355adb0145
Charlotte Bory <cmmbory@gmail.com>
2021-03-11 11:28:05 +0100

    setup parsing logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,33 @@
-require 'uri'
+require 'open-uri'
+require 'nokogiri'
 
 class ArticlesController < ApplicationController
+
+    skip_before_action :authenticate_user!, only: [ :create ]
+
   def create
+    article = Article.new()
+    article.url = article_url("encyclopedia.com", params[:article][:query])
+    article.save
   end
 
   private
 
-  def build_url(source, query)
-    parsed_query = query.split(' ').map { |word| URI.escape(word) }
+  # Build article url based on selected source and input query
+  def article_url(source, query)
+    parsed_query = query.split(' ').map { |word| URI.escape(word) }.join('+')
     if source == "encyclopedia.com"
-      url = "https://www.encyclopedia.com/gsearch?q=#{parsed_query}"
+      search_url = "https://www.encyclopedia.com/gsearch?q=#{parsed_query}"
+      article_url = get_top_article_encyclopedia(search_url)
     end
+    return article_url
+  end
+
+  # On encyclopedia.com, retrieve top search result url
+  def get_top_article_encyclopedia(url)
+    # TO DO - load page through headless chrome
+    page = Nokogiri::HTML(open(url))
+    top_link = page.css('a.gs-title').first
+    return top_link['href']
   end
 end

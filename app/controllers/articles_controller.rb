4eb34837d4005a1b7137081546e5802ba49e7466
Charlotte Bory <cmmbory@gmail.com>
2021-03-11 12:29:21 +0100

    retrieve top article info from wikipedia

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,31 +1,30 @@
-require 'open-uri'
-require 'nokogiri'
-
 class ArticlesController < ApplicationController
 
     skip_before_action :authenticate_user!, only: [ :create ]
 
   def create
-    article = Article.new()
-    article.url = article_url("wikipedia.com", params[:article][:query])
+    args = get_article_info("wikipedia.com", params[:article][:query])
+    article = Article.new(args)
     article.save
   end
 
   private
 
   # Build article url based on selected source and input query
-  def article_url(source, query)
+  def get_article_info(source, query)
     parsed_query = query.split(' ').map { |word| URI.escape(word) }.join('+')
     if source == "wikipedia.com"
-      "https://en.wikipedia.org/w/index.php?search=#{parsed_query}&title=Special:Search&fulltext=1&ns0=1"
-      search_url = "https://en.wikipedia.org/w/index.php?search=#{parsed_query}&title=Special:Search&fulltext=1&ns0=1"
-      article_url = get_top_article_wikipedia(search_url)
+      search_url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=#{parsed_query}"
+      article_info = get_top_article_wikipedia(search_url)
     end
-    return article_url
+    return article_info
   end
 
   # On wikipedia.com, retrieve top search result url
   def get_top_article_wikipedia(url)
-
+    response = JSON.parse(RestClient.get(url))
+    articles = response[1]
+    urls = response[3]
+    top_article = {title: articles.first, url: urls.first }
   end
 end

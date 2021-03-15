63fd68af7f9517ac775c00e0d796363010b0a07d
Charlotte Bory <cmmbory@gmail.com>
2021-03-15 23:56:41 +0100

    disable btn while input empty

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,10 +23,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { manageButton } from '../components/query_form';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  manageButton();
 });
 
 import "controllers"
